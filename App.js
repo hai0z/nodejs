ed5baf8cc311a1c303737e8e2b85f5f058341e41
haivip <hainguyen56211@gmail.com>
2020-11-28 20:06:21 +0700

    ok

--- App.js
+++ App.js
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {View } from 'react-native';
+import BasisFlatlist from './components/basisFlatlist'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends Component{
+  render(){
+    return(
+      <BasisFlatlist />
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
